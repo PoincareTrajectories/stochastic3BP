e399b057003f511afcc7f21684c77146c4a2c3da
benni <benjaminboschmann93@gmail.com>
2022-05-19 23:48:00 +0200

    Going for Monte Carlo

--- brownian.jl
+++ brownian.jl
@@ -17,9 +17,19 @@ function σ(du,u,p,t)
     return 1
 end
 
-sol, t = solve(μ, σ, u0, tspan, dt = 0.1)
-sol2, _ = solve(μ, σ, u0, tspan, dt = 0.1)
-sol3, _ = solve(μ, σ, u0, tspan, dt = 0.1)
-plot(t, [sol, sol2, sol3])
+# sol, t = solve(μ, σ, u0, tspan, dt = 0.1)
+# sol2, _ = solve(μ, σ, u0, tspan, dt = 0.1)
+# sol3, _ = solve(μ, σ, u0, tspan, dt = 0.1)
+# sol4, _ = solve(μ, σ, u0, tspan, dt = 0.1)
+
+sols = Vector{Vector{Number}}()
+ts = Vector{Any}()
+for i in 1:100
+    sol, t = solve(μ, σ, u0, tspan, dt = 0.1)
+    push!(sols, sol)
+    global ts = t
+end
+
+plot(ts, sols)
 date_string = Dates.format(now(), "YYYY_mm_dd-HH_MM")
-savefig("solutions" * date_string)
+savefig("MC-solutions" * date_string)
